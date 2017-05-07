d42fd5b29f431ae052f4c9fffa810daeb9d87ab2
zlikun <zlikun-dev@hotmail.com>
2017-05-07 09:21:52 +0800

    DESede 算法用例(未实现)

---
+++ zlikun-jee-crypto-symmetrical/src/test/java/com/zlikun/crypto/DESedeTest.java
@@ -0,0 +1,21 @@
+package com.zlikun.crypto;
+
+import org.junit.Test;
+
+/**
+ * DESede(三重DES)算法实现用例
+ * DESede = TripleDES = 3DES
+ * @author zlikun <zlikun-dev@hotmail.com>
+ * @date 2017/5/7 9:18
+ */
+public class DESedeTest {
+
+    @Test
+    public void des_ede() {
+
+        // TODO
+
+    }
+
+
+}
