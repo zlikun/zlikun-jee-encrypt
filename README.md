f52ab3d5ce8cc3d24757e7971cd1690e744a020b
zlikun <zlikun-dev@hotmail.com>
2017-05-07 09:17:07 +0800

    DES 算法实现

--- README.md
+++ README.md
@@ -7,5 +7,12 @@
 - zlikun-jee-encrypt-symmetrical 对称加密，AES等
 - zlikun-jee-encrypt-rsa 非对称加密  
 
+#### DES/DES3/AES
+> DES、AES 或者 3DES 属于块加密算法，一般来说原文必须是 8 的整数倍，所以块加密算法除了加密模式之外，还涉及到填充模式。  
+> 如果使用 NoPadding 的话，那么必须保证原文字节是 8 的倍数，否则的话需要使用其他的填充模式。  
+> JCE 提供另外两种填充模式：PKCS5Padding 和 ISO10126Padding
+
+参考资料：<http://bbs.csdn.net/topics/360204005#post-371051116>
+
 #### 参考资料
 - [让你完全理解base64是怎么回事](https://segmentfault.com/a/1190000004533485)
--- zlikun-jee-crypto-symmetrical/src/test/java/com/zlikun/crypto/DESTest.java
+++ zlikun-jee-crypto-symmetrical/src/test/java/com/zlikun/crypto/DESTest.java
@@ -1,6 +1,17 @@
 package com.zlikun.crypto;
 
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * DES加密算法实现测试
@@ -9,10 +20,78 @@ import org.junit.Test;
  */
 public class DESTest {
 
+    // 算法名称
+    private final String KEY_ALGORITHM = "DES";
+    // 算法名称/加密模式/填充方式
+    // DES共有四种工作模式-->>ECB：电子密码本模式、CBC：加密分组链接模式、CFB：加密反馈模式、OFB：输出反馈模式
+    // private final String CIPHER_ALGORITHM = "DES/ECB/NoPadding";
+    private final String CIPHER_ALGORITHM = "DES/ECB/ISO10126Padding";
+
+    // 实例化Cipher对象，它用于完成实际的加解密操作
+    private Cipher cipher ;
+
+    // 密钥字节信息(不直接使用SecretKey实例原因是为了模拟网络传输情形)
+    private byte [] secretKeyBytes ;
+
+    /**
+     * 初始化SecretKey实例
+     * @throws InvalidKeySpecException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+    @Before
+    public void init() throws NoSuchAlgorithmException, NoSuchPaddingException {
+
+        cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+
+        // Java6 只支持56位密钥，该模式下，将随机生成密钥
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM) ;
+        keyGenerator.init(56);
+        SecretKey secretKey = keyGenerator.generateKey() ;
+        this.secretKeyBytes = secretKey.getEncoded() ;
+
+        System.out.println(String.format("密钥：%s" ,Base64.encodeBase64String(this.secretKeyBytes)));
+    }
+
+    /**
+     * 将字节密钥还原成SecretKey实例
+     * @param key
+     * @return
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    SecretKey toKey(byte [] key) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException {
+        // 实例化DES密钥材料
+        DESKeySpec dks = new DESKeySpec(key);
+        // 实例化DES密钥工厂
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(KEY_ALGORITHM);
+        // 生成密钥
+        return keyFactory.generateSecret(dks);
+    }
+
     @Test
-    public void des() {
+    public void des() throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException, NoSuchProviderException {
+
+        // 要加密的字符串
+        final String message = "这是一个要加密的字符串。" ;
+
+        // I 加密
+
+        // 初始化Cipher对象，设置为加密模式
+        cipher.init(Cipher.ENCRYPT_MODE, toKey(this.secretKeyBytes), new SecureRandom());
+        // 执行加密操作
+        byte [] encryptBytes = cipher.doFinal(message.getBytes());
+        // 加密后的结果通常都会用Base64编码进行传输
+        System.out.println(String.format("加密后字符串(使用BASE64编码)：%s" ,Base64.encodeBase64String(encryptBytes)));
 
 
+        // II 解密
+
+        // 初始化Cipher对象，设置为解密模式
+        cipher.init(Cipher.DECRYPT_MODE, toKey(this.secretKeyBytes), new SecureRandom());
+        // 执行解密操作
+        Assert.assertEquals(message ,new String(cipher.doFinal(encryptBytes))) ;
 
     }
 
